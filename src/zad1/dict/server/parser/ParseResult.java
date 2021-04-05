28a3290d5242ff26ee40aaa438d9f5e2c485f3fd
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2021-04-06 00:03:31 +0200

    RequestParser logic

--- src/zad1/dict/server/parser/ParseResult.java
+++ src/zad1/dict/server/parser/ParseResult.java
@@ -3,12 +3,18 @@ package zad1.dict.server.parser;
 public class ParseResult {
     private boolean isValid;
     private String word;
+    private String sourceLanguage;
     private String targetLanguage;
     private Integer port;
 
-    public ParseResult(boolean isValid, String word, String targetLanguage, Integer port) {
+    public static ParseResult getInvalidParseResult() {
+        return new ParseResult(false, null, null, null, null);
+    }
+
+    public ParseResult(boolean isValid, String word, String sourceLanguage, String targetLanguage, Integer port) {
         this.isValid = isValid;
         this.word = word;
+        this.sourceLanguage = sourceLanguage;
         this.targetLanguage = targetLanguage;
         this.port = port;
     }
@@ -21,11 +27,15 @@ public class ParseResult {
         return word;
     }
 
+    public String getSourceLanguage() {
+        return sourceLanguage;
+    }
+
     public String getTargetLanguage() {
         return targetLanguage;
     }
 
-    public int getPort() {
+    public Integer getPort() {
         return port;
     }
 }
--- src/zad1/dict/server/parser/RequestParser.java
+++ src/zad1/dict/server/parser/RequestParser.java
@@ -1,15 +1,31 @@
 package zad1.dict.server.parser;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RequestParser {
     private static final String[] targetLanguages = {"EN", "FR", "DE", "RU", "ES"};
     private static final Pattern requestPattern = Pattern.compile(
-            "^\\{(\\w+),(\\w{2}),(\\d+)\\}$",
+            "^\\{(\\w+),(\\w{2}),([1-9]\\d+)\\}$",
             Pattern.CASE_INSENSITIVE
     );
 
     public static ParseResult parseRequest(String request) {
-        return new ParseResult(false, null, null, null);
+        Matcher matcher = requestPattern.matcher(request);
+        if (!matcher.find()) {
+            return ParseResult.getInvalidParseResult();
+        }
+
+        String targetLanguage = matcher.group(2);
+        if (!Arrays.asList(targetLanguages).contains(targetLanguage)) {
+            return ParseResult.getInvalidParseResult();
+        }
+
+        String word = matcher.group(1);
+        String sourceLanguage = "PL";
+        Integer port = Integer.parseInt(matcher.group(3));
+
+        return new ParseResult(true, word, sourceLanguage, targetLanguage, port);
     }
 }
